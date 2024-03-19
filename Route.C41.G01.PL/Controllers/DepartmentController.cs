2a6f4e134d8a85ae07f04fd11a25d608f6764835
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-03-19 22:19:55 +0200

    Part 11 Department Controller - Create

--- Route.C41.G01.PL/Controllers/DepartmentController.cs
+++ Route.C41.G01.PL/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Route.C41.G01.BLL.Interfaces;
 using Route.C41.G01.BLL.Repositories;
+using Route.C41.G01.DAL.Models;
 
 namespace Route.C41.G01.PL.Controllers
 {
@@ -19,6 +20,24 @@ namespace Route.C41.G01.PL.Controllers
         {
             var departments = _departmentRepository.GetAll();
             return View(departments);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        { 
+            if(ModelState.IsValid)
+            {
+                var Count = _departmentRepository.Add(department);
+                if (Count > 0)
+                    return RedirectToAction(nameof(Index));
+            }
+            return View(department);
         }
     }
 }
