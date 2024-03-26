0b505ea6a52d9ffd8b84a5e2e189dbba879d310b
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-03-26 12:29:47 +0200

    Part 07 Search

--- Route.C41.G01.PL/Controllers/EmployeeController.cs
+++ Route.C41.G01.PL/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Route.C41.G01.BLL.Interfaces;
@@ -7,142 +8,152 @@ using Route.C41.G01.DAL.Models;
 
 namespace Route.C41.G01.PL.Controllers
 {
-	public class EmployeeController : Controller
-	{
-		//private readonly IDepartmentRepository _departmentRepository;
-		private readonly IEmployeeRepository _employeeRepository;
+    public class EmployeeController : Controller
+    {
+        //private readonly IDepartmentRepository _departmentRepository;
+        private readonly IEmployeeRepository _employeeRepository;
 
-		private readonly IHostEnvironment _env;
+        private readonly IHostEnvironment _env;
 
-		public EmployeeController(IEmployeeRepository employeeRepository, /*IDepartmentRepository departmentRepository*/IHostEnvironment env) 
-		{
-			//_departmentRepository = departmentRepository;
-			_employeeRepository = employeeRepository;
-			_env = env;
-		}
+        public EmployeeController(IEmployeeRepository employeeRepository, /*IDepartmentRepository departmentRepository*/IHostEnvironment env)
+        {
+            //_departmentRepository = departmentRepository;
+            _employeeRepository = employeeRepository;
+            _env = env;
+        }
 
-		public IActionResult Index()
-		{
-			// if you want to keep the temp data in another redirection 
-			TempData.Keep(); 
-			// Bind Through View's Dictionary : Transfer Data from Action to View [one way]
-			// 1. viewData: is a Dictionary Type Property (introduced in ASP.Net Framwork 3.5)
-			//   => it help us to transfer data from Controller[Action] to view
-			ViewData["Message"] = "Hello From View Data";
+        public IActionResult Index(string searchInp)
+        {
+            // if you want to keep the temp data in another redirection 
+            TempData.Keep();
+            // Bind Through View's Dictionary : Transfer Data from Action to View [one way]
+            // 1. viewData: is a Dictionary Type Property (introduced in ASP.Net Framwork 3.5)
+            //   => it help us to transfer data from Controller[Action] to view
+            ViewData["Message"] = "Hello From View Data";
 
             // 2. viewBag: Dynamic type property (introduced in ASP.Net Framwork 4.0 based on dynamic type)
             //   => it help us to transfer data from Controller[Action] to view
 
             ViewBag.Message = "Hello From View Bag";
-			var employees = _employeeRepository.GetAll();
-			return View(employees);
-		}
+            var employees = Enumerable.Empty<Employee>();
 
-		public IActionResult Details(int? id, string ViewName = "Details")
-		{
-			if (!id.HasValue)
-				return BadRequest();
-			else
-			{
-				var employee = _employeeRepository.Get(id.Value);
-				if (employee is null)
-					return NotFound();
-
-				return View(ViewName, employee);
-			}
-		}
-
-		[HttpGet]
-		public IActionResult Create()
-		{
-			//ViewData["Departments"] = _departmentRepository.GetAll();
-			//ViewBag.Departments = _departmentRepository.GetAll();
-			return View();
-		}
-
-		[HttpPost]
-		public IActionResult Create(Employee employee) 
-		{
-            if (ModelState.IsValid)
+            if (String.IsNullOrEmpty(searchInp))
             {
-				// TempData: is Dictionary Type Property (introduced in ASP.net Framwork 3.5)
-				//         => used  to pass data between two consecutive requests.
-                var count = _employeeRepository.Add(employee);
-				if (count > 0)
-					TempData["Message"] = "Department Created Succeefuly";
-				
-				else
-                    TempData["Message"] = "An Error Occured";
-                
-					return RedirectToAction("Index");
+                employees = _employeeRepository.GetAll();
             }
-			return View(employee);
+            else
+            {
+                employees = _employeeRepository.GetEmployeesByName(searchInp);
+            }
+            
+           return View(employees);
         }
 
-		public IActionResult Edit(int? id) 
-		{
-			//ViewBag.Departments = _departmentRepository.GetAll();
-			return Details(id, "Edit");
-		}
+        public IActionResult Details(int? id, string ViewName = "Details")
+        {
+            if (!id.HasValue)
+                return BadRequest();
+            else
+            {
+                var employee = _employeeRepository.Get(id.Value);
+                if (employee is null)
+                    return NotFound();
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Edit([FromRoute]int id,Employee employee)
-		{
-			if(id != employee.Id)
-				return BadRequest();
+                return View(ViewName, employee);
+            }
+        }
 
-			if (!ModelState.IsValid)
-				return View(employee);
-			else
-			{
-				try
-				{
-					_employeeRepository.Update(employee);
-					return RedirectToAction("Index");
-				}
-				catch (Exception ex)
-				{
-					// 1. Log Exeption
-					// 2. Friendly Message
+        [HttpGet]
+        public IActionResult Create()
+        {
+            //ViewData["Departments"] = _departmentRepository.GetAll();
+            //ViewBag.Departments = _departmentRepository.GetAll();
+            return View();
+        }
 
-					if (_env.IsDevelopment())
-						ModelState.AddModelError(String.Empty, ex.Message);
-					else
-						ModelState.AddModelError(String.Empty, "An Error Has Occured During updating the Employee");
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                // TempData: is Dictionary Type Property (introduced in ASP.net Framwork 3.5)
+                //         => used  to pass data between two consecutive requests.
+                var count = _employeeRepository.Add(employee);
+                if (count > 0)
+                    TempData["Message"] = "Department Created Succeefuly";
 
-					return View(employee);
-				}
-			}
+                else
+                    TempData["Message"] = "An Error Occured";
 
-		}
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            //ViewBag.Departments = _departmentRepository.GetAll();
+            return Details(id, "Edit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, Employee employee)
+        {
+            if (id != employee.Id)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return View(employee);
+            else
+            {
+                try
+                {
+                    _employeeRepository.Update(employee);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    // 1. Log Exeption
+                    // 2. Friendly Message
+
+                    if (_env.IsDevelopment())
+                        ModelState.AddModelError(String.Empty, ex.Message);
+                    else
+                        ModelState.AddModelError(String.Empty, "An Error Has Occured During updating the Employee");
+
+                    return View(employee);
+                }
+            }
+
+        }
 
 
-		public IActionResult Delete(int? id) 
-		{
-			return Details(id, "Delete");
-		}
-		[HttpPost]
-		public IActionResult Delete(Employee employee)
-		{
-			 
-			try
-			{
-				_employeeRepository.Delete(employee);
-				return RedirectToAction("index");
-			}
-			catch(Exception ex)
-			{
-				// 1. Log Exeption
-				// 2. Friendly Message
+        public IActionResult Delete(int? id)
+        {
+            return Details(id, "Delete");
+        }
+        [HttpPost]
+        public IActionResult Delete(Employee employee)
+        {
 
-				if (_env.IsDevelopment())
-					ModelState.AddModelError(String.Empty, ex.Message);
-				else
-					ModelState.AddModelError(String.Empty, "An Error Has Occured During deleting the Employee");
+            try
+            {
+                _employeeRepository.Delete(employee);
+                return RedirectToAction("index");
+            }
+            catch (Exception ex)
+            {
+                // 1. Log Exeption
+                // 2. Friendly Message
 
-				return View(employee);
-			}
-		}
-	}
+                if (_env.IsDevelopment())
+                    ModelState.AddModelError(String.Empty, ex.Message);
+                else
+                    ModelState.AddModelError(String.Empty, "An Error Has Occured During deleting the Employee");
+
+                return View(employee);
+            }
+        }
+    }
 }
--- Route.C41.G01.PL/wwwroot/js/site.js
+++ Route.C41.G01.PL/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("#searchInp").on("keyup", async function () {
+    
+    let searchInpValue = $(this).val();
+    let url = `https://localhost:44309/Employee/Index?searchInp=${searchInpValue}`;
+
+    //using fetch
+    let Response = await fetch(url,
+        {
+            Method: 'POST',
+        });
+    let text = await Response.text();
+    console.log(text);
+
+    //let xhr = new XMLHttpRequest();
+    //xhr.open("Get", url);
+    //xhr.onreadystatechange = function () {
+    //    if (this.readyState == 4 && this.status == 200)
+    //    {
+    //        console.log(this.responseText);
+    //    }
+    //}
+    //xhr.send();
+})
