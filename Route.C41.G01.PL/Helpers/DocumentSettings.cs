70c9fdf230be8a967d619ffb9f31d80ffcd11e15
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-04-01 18:16:58 +0200

    Part 01 Document Setting

---
+++ Route.C41.G01.PL/Helpers/DocumentSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace Route.C41.G01.PL.Helpers
+{
+	public static class DocumentSettings
+	{
+		public static string UploadFile(IFormFile file, string folderName)
+		{
+			//1. Get Located Folder Path
+			//string FolderPath = $"{Directory.GetCurrentDirectory()}\\wwwroot\\files\\{folderName}"; 
+			string FolderPath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroor\\files",folderName);
+
+			if (!Directory.Exists(FolderPath))
+				Directory.CreateDirectory(FolderPath);
+
+			//2. Get FileName and make it UNIQE
+			// file.Name;  extextion .jpg
+			string fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+			//3. Get File Path
+			string filePath = Path.Combine(FolderPath, fileName);
+
+			//4. Save File as Streams [Data Per Time]
+			var FileStream = new FileStream(filePath,FileMode.Create);
+
+			file.CopyTo(FileStream);
+
+			return fileName;
+		}
+
+		public static void DeleteFile(string fileName, string folderName)
+		{
+			string filePath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot\\files",folderName,fileName);
+			if(File.Exists(filePath))
+				File.Delete(filePath);	
+
+		}
+	}
+}
