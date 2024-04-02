1d4cd121ba25064863ad9c886a2c062702218bf2
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-04-02 03:07:48 +0200

    Part 07 Microsoft Identity Package

--- Route.C41.G01.DAL/Data/ApplicationDbContext.cs
+++ Route.C41.G01.DAL/Data/ApplicationDbContext.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Route.C41.G01.DAL.Data.Configurations;
 using Route.C41.G01.DAL.Models;
 
 namespace Route.C41.G01.DAL.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext 
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
         {
@@ -26,11 +28,13 @@ namespace Route.C41.G01.DAL.Data
         {
 			// modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
 			// modelBuilder.ApplyConfiguration<Employee>(new EmployeeConfigurations());
-
+            base.OnModelCreating(modelBuilder);
 			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
 
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
+
+
     }
 }
