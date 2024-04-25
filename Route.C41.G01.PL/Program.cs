1fbc51d2c773ee1866e94f5ae5ef9653177f294f
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2024-04-25 19:00:39 +0200

    Part 07 Upgrade to .NET 8.0

--- Route.C41.G01.PL/Program.cs
+++ Route.C41.G01.PL/Program.cs
@@ -2,10 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Route.C41.G01.DAL.Data;
+using Route.C41.G01.DAL.Models;
+using Route.C41.G01.PL.Extensions;
+using Route.C41.G01.PL.Helpers;
+using Route.C41.G01.PL.Services.EmailSender;
 
 namespace Route.C41.G01.PL
 {
@@ -13,14 +22,78 @@ namespace Route.C41.G01.PL
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+			#region configure services
+			var WebApplicationBuilder = WebApplication.CreateBuilder(args);
+			WebApplicationBuilder.Services.AddControllersWithViews();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+			//services.AddScoped<ApplicationDbContext>();
+			//services.AddScoped<DbContextOptions<ApplicationDbContext>>();
+
+			WebApplicationBuilder.Services.AddDbContext<ApplicationDbContext>(
+				options => {
+					options.UseSqlServer(WebApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
+				}/*,ServiceLifetime.Singleton*/);
+			//ApplicationServicesExtensions.AddApplicationServices(services); Static Method
+
+			//Extention Method
+			WebApplicationBuilder.Services.AddApplicationServices();
+
+			WebApplicationBuilder.Services.AddAutoMapper(M => M.AddProfile(new MappingProfiles()));
+			WebApplicationBuilder.Services.AddIdentity<ApplicationUser, IdentityRole>(
+				options =>
+				{
+					options.Password.RequiredLength = 5;
+					options.Password.RequireNonAlphanumeric = false;
+					options.Password.RequireUppercase = false;
+					options.Password.RequireLowercase = false;
+
+					options.Lockout.AllowedForNewUsers = true;
+					options.Lockout.MaxFailedAccessAttempts = 5;
+					options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromHours(2);
+
+					options.User.RequireUniqueEmail = true;
+				}).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+			WebApplicationBuilder.Services.ConfigureApplicationCookie(options =>
+			{
+				options.AccessDeniedPath = "/Home/Error";
+				options.LoginPath = "/Account/SignIn";
+				options.ExpireTimeSpan = TimeSpan.FromDays(1);
+			});
+			WebApplicationBuilder.Services.AddAuthentication();
+			WebApplicationBuilder.Services.AddTransient<IEmailSender, EmailSender>();
+
+			var app = WebApplicationBuilder.Build();
+			#endregion
+
+			#region configre kestrel middlewares
+			if (WebApplicationBuilder.Environment.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+			});
+			#endregion
+
+			app.Run();
+		}
     }
 }
